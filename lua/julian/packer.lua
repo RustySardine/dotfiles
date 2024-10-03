621967b8ed1aa03206d354c922175f5e625bfa79
julian.moore <julian@DESKTOP-14E7FA6>
2024-10-03 15:26:56 +0100

    added lua formatter

--- lua/julian/packer.lua
+++ lua/julian/packer.lua
@@ -1,42 +1,51 @@
 -- This file can be loaded by calling `lua require('plugins')` from your init.vim
 
 -- Only required if you have packer configured as `opt`
-vim.cmd [[packadd packer.nvim]]
+vim.cmd([[packadd packer.nvim]])
 
-return require('packer').startup(function(use)
+return require("packer").startup(function(use)
 	-- Packer can manage itself
 
-	use 'wbthomason/packer.nvim'
-	use 'mhartington/formatter.nvim'
-	use 'voldikss/vim-floaterm'
-	use {
-		'nvim-telescope/telescope.nvim', tag = '0.1.8',
+	use("wbthomason/packer.nvim")
+	use({
+		"stevearc/conform.nvim",
+		config = function()
+			require("conform").setup({
+				formatters_by_ft = {
+					lua = { "stylua" },
+				},
+			})
+		end,
+	})
+	use("voldikss/vim-floaterm")
+	use({
+		"nvim-telescope/telescope.nvim",
+		tag = "0.1.8",
 		-- or                            , branch = '0.1.x',
-		requires = { {'nvim-lua/plenary.nvim'} }
-	}
-	
+		requires = { { "nvim-lua/plenary.nvim" } },
+	})
 
 	use({
-		'rose-pine/neovim',
-		as = 'rose-pine',
+		"rose-pine/neovim",
+		as = "rose-pine",
 		config = function()
-			vim.cmd('colorscheme rose-pine')
-		end
-	})	
+			vim.cmd("colorscheme rose-pine")
+		end,
+	})
 
-	use{
-		'nvim-treesitter/nvim-treesitter',
+	use({
+		"nvim-treesitter/nvim-treesitter",
 		run = function()
-			local ts_update = require('nvim-treesitter.install').update({ with_sync = true })
+			local ts_update = require("nvim-treesitter.install").update({ with_sync = true })
 			ts_update()
 		end,
-	}
-	use 'simrat39/rust-tools.nvim'
-	use('mbbill/undotree')
-	use('m4xshen/autoclose.nvim')
-	use('tpope/vim-fugitive')
-	use {
-		'neovim/nvim-lspconfig',
+	})
+	use("simrat39/rust-tools.nvim")
+	use("mbbill/undotree")
+	use("m4xshen/autoclose.nvim")
+	use("tpope/vim-fugitive")
+	use({
+		"neovim/nvim-lspconfig",
 		requires = {
 			"williamboman/mason.nvim",
 			"williamboman/mason-lspconfig.nvim",
@@ -48,6 +57,6 @@ return require('packer').startup(function(use)
 			"L3MON4D3/LuaSnip",
 			"saadparwaiz1/cmp_luasnip",
 			"j-hui/fidget.nvim",
-		}
-	}
+		},
+	})
 end)
